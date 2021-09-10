d140e98b809ac5293808e8a1cd5f061260b16d82
MatthewMion <87820540+ShmoneJones@users.noreply.github.com>
2021-09-10 10:22:19 -0400

    displayed name and age on screen

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+
+
+
+const WrapperCharacter=styled.div`
+`
 // Write your Character component here
+const Character = (props) =>{
+    const {swPeopleList} = props;
+    console.log(swPeopleList)
+    return (
+        <WrapperCharacter>
+            {swPeopleList.map(person =>{
+                return (
+                <div>
+                    <h2>{person.name}</h2>
+                    <span>{person.birth_year}</span>
+                </div>
+                )
+            })}
+        </WrapperCharacter>
+
+
+
+
+    )
+}
+
+
+
+export default Character;
