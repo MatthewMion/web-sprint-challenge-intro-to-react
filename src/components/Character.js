df60a1e0358ac7522c5d89697e73bd7195918811
MatthewMion <87820540+ShmoneJones@users.noreply.github.com>
2021-09-10 11:37:21 -0400

    finished rough styling

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,14 +1,39 @@
 import React from "react";
 import styled from "styled-components";
-
+import theme from "../theme";
 
 
 const WrapperCharacter=styled.div`
+display:flex;
+flex-direction:column;
+width:100%;
+text-align:center;
+/* border: 3px solid red; */
+align-items: center;
+div{
+    display:flex;
+    width: 75%;
+    padding:2%;
+    margin:2%;
+    border:3px solid ${theme.primaryColor};
+    align-items:center;
+    justify-content: space-between
+}
+h2{
+    color:${theme.secondaryColor}
+}
+span{
+    color:${theme.primaryColor};
+    background-color:${theme.tertiaryColor};
+    font-weight: bold;
+    padding:1%;
+    border-radius: 15% 35% 15% 35%;
+}
 `
 // Write your Character component here
 const Character = (props) =>{
     const {swPeopleList} = props;
-    console.log(swPeopleList)
+    // console.log(swPeopleList)
     return (
         <WrapperCharacter>
             {swPeopleList.map(person =>{
---
+++ src/theme.js
@@ -0,0 +1,7 @@
+const theme = {
+    primaryColor: '#BA6A1A',
+    secondaryColor:'#1BBB6B',
+    tertiaryColor:'#6B1BBB',
+}
+
+export default theme
